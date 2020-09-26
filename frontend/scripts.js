9b55ad578409c90082168d1ce88d0abadcbe859e
Sergio Cosman <im.serge@hotmail.com>
2020-09-26 15:26:23 -0400

    Added text color, and enter key to send message

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,8 +1,26 @@
 function sendMessage()
 {
-    const $msg = $("input#user-message").val();
-    const $newMessage = $("<p>", {class: "Message", text: $msg });
+    const $msg = $("input#user-name").val() + ": " + $("input#user-message").val();
+    const $textColor = $("input#user-message").css("color");
+    const $newMessage = $("<p>", {class: "Message", text: $msg, style: `color: ${$textColor}`});
     
     $("#chatbox").append($newMessage);
     $("input#user-message").val("");
 }
+
+function colorSwap(colorbox)
+{
+    const $color = $(colorbox).css("background-color");
+    $("input#user-message").css("color", $color);
+    
+}
+
+$(document).ready( () => {
+    $("input#user-message").keypress( (event) => {
+        if (event.keyCode == '13')
+        {
+            sendMessage();
+        }
+    });
+
+});
